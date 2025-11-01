95c2691680d6035d5acc33740c2bf431f5c48009
muhammad <muhammad_09>
2025-11-01 17:45:07 +0500

    muhammad_bot

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const TOKEN ="7750435953:AAFfzgcSIuRz5bzXixjrjZex4C5-7YOQoaI";
+const TOKEN = "7750435953:AAFfzgcSIuRz5bzXixjrjZex4C5-7YOQoaI";
 
-const bot = new TelegramBot(TOKEN, { polling: true});
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on("message", function (msg)) {
+bot.on("message", function (msg) {
     const chatid = msg.chat.id;
     const text = msg.text;
 
     bot.sendMessage(chatid, `Solom --> ${text}`);
     console.log("Xabar keldi");
     console.log(msg);
-};
+});
 
 console.log("Botimiz ishga tushdi")
