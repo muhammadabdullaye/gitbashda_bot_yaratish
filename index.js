b5763638075ec5cd04c207198bbbbe5b84ad5baa
muhammad <muhammad_09>
2025-11-06 00:12:46 +0500

    bot

--- index.js
+++ index.js
@@ -4,13 +4,38 @@ const TOKEN = "7750435953:AAFfzgcSIuRz5bzXixjrjZex4C5-7YOQoaI";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on("message", function (msg) {
-    const chatid = msg.chat.id;
-    const text = msg.text;
+bot.on("message", async function (msg) {
+  console.log(msg);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstName = msg.chat.first_name;
 
-    bot.sendMessage(chatid, `Solom --> ${text}`);
-    console.log("Xabar keldi");
-    console.log(msg);
+  if (text == "/start") {
+    bot.sendMessage(chatId, `Xush kelibsiz, ${firstName} 👋`, {
+      reply_markup: {
+        keyboard: [
+          [{ text: "Boshlash 🔥" }],
+          [{ text: "Menu 🍔" }, { text: "Sozlamalar ⚙️" }],
+        ],
+        resize_keyboard: true,
+      },
+    });
+  } else if (text == "Boshlash 🔥") {
+    const xabar = await bot.sendMessage(chatId, "Iltimos, kuting...");
+
+    setTimeout(function () {
+      bot.deleteMessage(chatId, xabar.message_id);
+      // bot.sendMessage(chatId, "1 sekund kechikdi");
+      bot.sendPhoto(chatId, "./images.jfif", {
+        caption: ` The Urus has a V8 twin-turbocharged engine with a maximum power output of 650 hp, and offers the best power-to-weight ratio in the SUV segment. `,
+      });
+    }, 1000);
+  } else if (text == "Menu 🍔") {
+    bot.sendMessage(chatId, "Menyuga xush kelibsiz");
+  }
+
+  console.log("Start");
+  console.log("Xabar keldi");
 });
 
-console.log("Botimiz ishga tushdi")
+console.log("Botimiz ishga tushdi");
